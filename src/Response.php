f3b288b92b8f08d7408c3d2ba1d46c349407aff8
Cédric Ducarre <samray1024@gmail.com>
2023-11-01 19:38:38 +0100

    Fix EOL of headers string

--- src/Response.php
+++ src/Response.php
@@ -441,7 +441,7 @@ class Response
 		$sHeaders = '';
 
 		foreach ($this->aHeaders as $sName => $sValue)
-			$sHeaders .= $sName .': '. $sValue ."r\n";
+			$sHeaders .= $sName .': '. $sValue ."\r\n";
 
 		return $sHeaders;
 	}
